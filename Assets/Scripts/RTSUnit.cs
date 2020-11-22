98c0ffaee02a2df49ad3b65b0d74dbacf5e5c49a
Cody Palin <codypalin@u.boisestate.edu>
2020-11-21 21:48:44 -0800

    Unit rotation
    units rotate before moving. if the angle is small enough they will rotate before moving.

--- Assets/Scripts/RTSUnit.cs
+++ Assets/Scripts/RTSUnit.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class RTSUnit : MonoBehaviour
 {
-    private float speed = (float)0.1 * 3;
+    private float speed = 0.1f * 3;
     private GameObject selectedGameObject;
     private Vector3 MovementStart;
     private Vector3 MovementDestination;
+    private Quaternion RotationDestination;
+    private float RotationAngle = 0;
     private bool Moving;
+    private bool Rotating;
     private void Awake()
     {
         selectedGameObject = transform.Find("selected").gameObject;
@@ -20,9 +23,18 @@ public class RTSUnit : MonoBehaviour
     }
     private void Update()
     {
-        if (Moving)
+        if (Rotating)
         {
-            transform.position = Vector3.MoveTowards(transform.position, MovementDestination, Time.deltaTime*speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,RotationDestination, speed*100 * Time.deltaTime);
+            if (transform.rotation == RotationDestination)
+            {
+                Rotating = false;
+                Debug.Log("Done Rotating");
+            }
+        }
+        if (Moving && (!Rotating || RotationAngle<40))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MovementDestination, speed * Time.deltaTime);
             if(MovementDestination == transform.position)
             {
                 Debug.Log("Done Moving");
@@ -38,6 +50,11 @@ public class RTSUnit : MonoBehaviour
     {
         MovementStart = transform.position;
         MovementDestination = destination;
+        Vector3 targetDirection = MovementDestination - MovementStart;
+        RotationDestination = Quaternion.LookRotation(Vector3.forward, targetDirection);
+        RotationAngle = Quaternion.Angle(RotationDestination,transform.rotation);
+        Debug.Log("Moving");
+        Rotating = true;
         Moving = true;
     }
 }
